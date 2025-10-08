5cd29c2a689faa62fd353ae656f5e24601c07059
Unique Chukwudi <uniquechukwudi@Uniques-MacBook-Pro.local>
2025-10-08 12:26:11 -0400

    Auto-load pizza data on startup

--- pizzaking_backend/wsgi.py
+++ pizzaking_backend/wsgi.py
@@ -8,12 +8,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-from load_pizzas_startup import run as load_pizzas_startup
-load_pizzas_startup()
-
+# 1. Set Django settings FIRST
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizzaking_backend.settings')
 
+# 2. Initialize Django application (this sets up everything)
 application = get_wsgi_application()
+
+# 3. NOW import and call your startup function (after Django is ready)
+from load_pizzas_startup import run as load_pizzas_startup
+load_pizzas_startup()
